edd5534d8e6945bb11d73be565cb56c3488adfe0
Vignesh D <vignesh.d9123@gmail.com>
2026-07-05 00:23:33 +0530

    Add http/s ingress blocks

--- infra-setup.tf
+++ infra-setup.tf
@@ -31,6 +31,22 @@ resource "aws_security_group" "allow_ssh" {
         cidr_blocks = ["0.0.0.0/0"] // TODO: Will add var using github repo var
     }
 
+    ingress {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      description = "Allow HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port   = 0
         to_port     = 0
